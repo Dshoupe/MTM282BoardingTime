f098b01b9904420373a56ebf8397cc90eac762c3
DShoupe <dylanshoupe@yahoo.com>
2018-02-16 08:45:28 -0700

    Added some mongoose stuff

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ app.set('view engine', 'pug');
 app.set('views', `${__dirname}/views`);
 app.use(express.static(path.join(`${__dirname}/public`)));
 
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/data');
+
+var mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error:'));
+mdb.once('open', function (callback) {
+
+});
+
+var personSchema = mongoose.Schema({
+  username: String,
+  avatarImg: String,
+  passHash: String,
+  userLevel: String,
+  email: String,
+  age: Number
+});
+
+var Person = mongoose.model('People_Collection', personSchema);
+
 var urlencodedParser = bodyParser.urlencoded({
     extended: true
 });
